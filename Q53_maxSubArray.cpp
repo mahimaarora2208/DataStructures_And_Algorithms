306599ccccb85c982b78798cf22c804202398c28
mahimaarora2208 <marora1@umd.edu>
2023-01-30 20:04:48 -0500

    pseudocode for kadane's algorithm

--- Q53_maxSubArray.cpp
+++ Q53_maxSubArray.cpp
@@ -1,6 +1,16 @@
 /**
  * Kadane's Algorithm - an interative dynamic programming method
- * nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * nums = [1,-2,3,4,-5,6]
+ * -------------
+ * Pseudocode
+ * ------------- 
+ *  
+ *  - Declare a variable which holds the max sum [maxVal]
+ *  - Read the vector using for loop for(i < len(nums))
+ *  - Change i th value in vector with max --> max(nums[i], nums[i-1]) : here nums[i-1] will be sum until that ith value
+ *  - Change maxVal variable if nums[i] > maxVal
+ *  - Keep going until end 
+ *  - return maxVal
  * 
  */
 
@@ -22,7 +32,7 @@ int maxSubArray(vector<int> &nums)
 int main(int argc, char const *argv[])
 {
     /* code */
-    vector<int> input{-2,1,-3,4,-1,2,1,-5,4};
+    vector<int> input{1,-2,3,4,-5,6}; //-2,1,-3,4,-1,2,1,-5,4
     int result = maxSubArray(input);
     cout << "Result: " << result;
     return 0;
